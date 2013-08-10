f856bb38f7f1a35f94a692fad1d9aa8ab6e57356
Takumi Akiyama <t.akiym@gmail.com>
2013-08-10 11:13:01 +0900

    fix for perl-5.13.6 or less
    Sequence (?^...) not recognized in regex

--- lib/Regexp/VerbalExpressions.pm
+++ lib/Regexp/VerbalExpressions.pm
@@ -14,10 +14,12 @@ sub new {
     return bless {
         s         => '',
         modifiers => {
-            i => '',
-            g => '',
-            m => '',
+            i => 0,
+            m => 0,
+            s => 0,
+            x => 0,
         },
+        global_matching => 0,
     }, $class;
 }
 
@@ -31,8 +33,15 @@ sub add {
 
 sub regex {
     my $self = shift;
-    my $modifiers = join '', values %{$self->{modifiers}};
-    return qr/(?^$modifiers:$self->{s})/;
+    my $enabled = my $disabled = '';
+    for my $modifier (sort keys %{$self->{modifiers}}) {
+        if ($self->{modifiers}{$modifier}) {
+            $enabled .= $modifier;
+        } else {
+            $disabled .= $modifier;
+        }
+    }
+    return qr/(?$enabled-$disabled:$self->{s})/;
 }
 *compile = \&regex;
 
@@ -119,32 +128,44 @@ sub or {
 
 sub replace {
     my ($self, $source, $value) = @_;
-    if ($self->{modifiers}{g}) {
-        local $self->{modifiers}{g} = ''; # not include /g in pattern
-        my $re = $self->regex;
+    my $re = $self->regex;
+    if ($self->{global_matching}) {
         $source =~ s/$re/$value/g;
     } else {
-        my $re = $self->regex;
         $source =~ s/$re/$value/;
     }
     return $source;
 }
 
+sub _build_modifier {
+    my ($self, $modifier, $value) = @_;
+    if (!defined $value) {
+        # XXX verex->with_any_case;
+        $self->{modifiers}{$modifier} = 1;
+    } else {
+        $self->{modifiers}{$modifier} = $value ? 1 : 0;
+    }
+}
+
 sub with_any_case {
     my ($self, $value) = @_;
-    $self->{modifiers}{i} = $value ? 'i' : '';
+    $self->_build_modifier(i => $value);
     return $self;
 }
 
 sub stop_at_first {
     my ($self, $value) = @_;
-    $self->{modifiers}{g} = $value ? 'g' : '';
+    if (!defined $value) {
+        $self->{global_matching} = 1;
+    } else {
+        $self->{global_matching} = $value ? 1 : 0;
+    }
     return $self;
 }
 
 sub search_one_line {
     my ($self, $value) = @_;
-    $self->{modifiers}{m} = $value ? 'm' : '';
+    $self->_build_modifier(m => $value);
     return $self;
 }
 
